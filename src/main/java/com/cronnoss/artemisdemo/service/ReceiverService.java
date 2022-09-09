a723ccf842f061196ea3810a3ab534503fdd8e17
VTB4052159 <egkarasev@vtb.ru>
2022-09-09 12:40:14 +0300

    first commit

---
+++ src/main/java/com/cronnoss/artemisdemo/service/ReceiverService.java
@@ -0,0 +1,17 @@
+package com.cronnoss.artemisdemo.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReceiverService {
+
+    Logger log = LoggerFactory.getLogger(ReceiverService.class);
+
+    @JmsListener(destination = "${jms.queue}")
+    private void receiveMessage(String message) {
+        log.info("Received message: " + message);
+    }
+}
